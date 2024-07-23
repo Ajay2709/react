e6710b32dc7090349c866490a247281cdd5698f1
ajay.rk <ajay.rk@zohocorp.com>
2024-07-23 08:23:53 +0530

    added comments

--- asyncthunkapp/src/fetchDataThunk.js
+++ asyncthunkapp/src/fetchDataThunk.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// Define the async thunk
+/* 
+1. Define the async thunk
+2. Create the Slice with Async Thunk. - dataSlice.js
+3. Configure the Store - store.js
+4. Provide the Store to Your Application - index.js
+5. Use the Async Thunk in a Component - DataList.js
+6. Use the Connected Component in App - App.js
+*/
+
 const fetchData = createAsyncThunk('data/fetchData', async () => {
     const response = await axios.get('https://api.example.com/data');
     return response.data;
